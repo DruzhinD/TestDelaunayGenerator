452352302835496cfff3a24d0b21409daeff43e7
DruzhinD <d.druzhin04@gmail.com>
2025-08-31 16:16:45 +1000

    тестирование BenchmarkDotNet

---
+++ BenchmarkTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //BenchmarkRunner.Run<BenchmarkTestClass>();
+            BenchmarkRunner.Run<RestoreBorderBenchmark>();
+        }
+    }
+}
